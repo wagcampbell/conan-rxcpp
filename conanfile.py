6896dd19c2f19c411e1fa7a1275814a92febf5b6
Uilian Ries <uilianries@gmail.com>
2018-11-17 11:17:31 -0200

    #542 Bump version to 4.1.0
    - Apply Conan conventions
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from conans import ConanFile, tools
 import os
+from conans import ConanFile, tools
 
 
 class RxcppConan(ConanFile):
     name = "rxcpp"
-    version = "4.0.0"
+    version = "4.1.0"
     url = "https://github.com/bincrafters/conan-rxcpp"
+    homepage = "https://github.com/ReactiveX/RxCpp"
+    author = "Bincrafters <bincrafters@gmail.com>"
     description = "Library for composing operations on streams of asynchronous events."
     license = "Apache-2.0"
+    topics = ("conan", "rxcpp", "reactivex", "algorithms", "values-distributed-in-time")
     exports = ["LICENSE.md"]
-    root = "RxCpp-" + version
+    _source_subfolder = "source_subfolder"
 
     def source(self):
         source_url = "https://github.com/Reactive-Extensions/RxCpp"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        extracted_folder = "RxCpp-" + self.version
+        os.rename(extracted_folder, self._source_subfolder)
 
     def package(self):
-        header_dir = os.path.join(self.root, "Rx", "v2", "src")
+        self.copy("license.md", dst="licenses", src=self._source_subfolder)
+        header_dir = os.path.join(self._source_subfolder, "Rx", "v2", "src")
         self.copy(pattern="*", dst="include", src=header_dir)
 
     def package_id(self):
